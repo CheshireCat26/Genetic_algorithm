c2feaf2b58c14eeac0b6976f49d5109601294fd6
CheshireCat <nail160306@gmail.com>
2019-11-14 18:20:39 +0300

    Implemented initialization part of the algorithm

--- Genetic_algorithm.cpp
+++ Genetic_algorithm.cpp
@@ -4,20 +4,39 @@
 
 #include "Genetic_algorithm.h"
 #include <stdexcept>
+#include <ctime>
 
 using namespace std;
 
-Genetic_algorithm::Genetic_algorithm(double gMax, double gMin, double (*fitnessCalc)(std::vector<double>),
+Genetic_algorithm::Genetic_algorithm(double gMax, double gMin, double (*fitnessCalc)(vector<double>&),
                                      unsigned int crossoverP, double classicMutChan, double sigmaMutChan,
-                                     unsigned int populationSize) : g_max(gMax), g_min(gMin), fitness_calc(fitnessCalc),
-                                                                    crossover_p(crossoverP),
-                                                                    classic_mut_chan(classicMutChan),
-                                                                    sigma_mut_chan(sigmaMutChan),
-                                                                    population_size(populationSize)
+                                     unsigned int populationSize, unsigned int chromosomeSize)
+                                     : g_max(gMax), g_min(gMin), fitness_calc(fitnessCalc), crossover_p(crossoverP),
+                                       classic_mut_chan(classicMutChan), sigma_mut_chan(sigmaMutChan),
+                                       population_size(populationSize), chromosome_size(chromosomeSize),
+                                       distribution(g_min, g_max)
 {
+    random_engine.seed(rd());
     if (gMax < gMax)
         throw runtime_error("Genetic_algorithm::Genetic_algorithm(): gMax must be <= gMin");
     if (classicMutChan + sigmaMutChan > 1)
         throw runtime_error("Genetic_algorithm::Genetic_algorithm(): classicMutChan + sigmaMutChan must be <= 1");
+
+    initialization();
 }
 
+void Genetic_algorithm::initialization() {
+    for (int i = 0; i < population_size; i++)
+    {
+        Individual individual(chromosome_size);
+        for (int j = 0; j < chromosome_size; j++)
+        {
+            individual.chromosome[j] = distribution(random_engine);
+        }
+        population.emplace_back(individual);
+    }
+}
+
+const vector<Individual> &Genetic_algorithm::getPopulation() const {
+    return population;
+}
--- Genetic_algorithm.h
+++ Genetic_algorithm.h
@@ -6,6 +6,7 @@
 #define GENETIC_ALGORITHM_GENETIC_ALGORITHM_H
 
 #include <vector>
+#include <random>
 
 class Individual
 {
@@ -13,18 +14,23 @@ public:
     std::vector<double> chromosome;
     double fitness;
     double chance; // is chance to be selected at crossover
+
+    explicit Individual(unsigned int chrom_size) : fitness{0}, chance{0}, chromosome(chrom_size) { }
 };
 
 class Genetic_algorithm {
 public:
-    Genetic_algorithm(double gMax, double gMin, double (*fitnessCalc)(std::vector<double>), unsigned int crossoverP,
-                      double classicMutChan, double sigmaMutChan, unsigned int populationSize);
+    Genetic_algorithm(double gMin, double gMax, double (*fitnessCalc)(std::vector<double>&), unsigned int crossoverP,
+                      double classicMutChan, double sigmaMutChan, unsigned int populationSize,
+                      unsigned int chromosomeSize);
+
+    const std::vector<Individual> &getPopulation() const;
 
 private:
     //g_max >= g_min
     double g_max; // gene upper bound
     double g_min; // gene lower bound
-    double (*fitness_calc) (std::vector<double>);
+    double (*fitness_calc) (std::vector<double>&);
     unsigned int crossover_p; //amount of cut points at crossover. >= 1;
     //classic_mut_chan + sigma_mut_chan <= 1
     double classic_mut_chan; //chance of classic mutation
@@ -32,6 +38,14 @@ private:
     unsigned int population_size;
     std::vector<Individual> population;
     std::vector<Individual*> child; //child at last iteration
+    unsigned int chromosome_size;
+
+    std::random_device rd;
+    std::default_random_engine random_engine;
+    std::uniform_real_distribution<double> distribution;
+
+
+    void initialization();
 };
 
 
